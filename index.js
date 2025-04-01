08d60db02cd754b299394debcc745cbdea73276b
arturrg-ingenix <aroig@ingenix.es>
2025-04-01 10:34:41 +0200

    Forzar redeploy corrigiendo estructura

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
+require('dotenv').config(); // 👈 Agrega esta línea al principio
 
 const app = express();
 const port = process.env.PORT || 3000; // Soporte para despliegue online
